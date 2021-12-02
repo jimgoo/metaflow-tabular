14fea2c9e8aa9b823dfd27336bb70ec8591d536f
jimgoo <jimmiegoode@gmail.com>
2021-12-02 11:46:18 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,41 +4,40 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+requirements = []
 
-requirements = [ ]
-
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Jimmie Goode",
-    author_email='jimmiegoode@gmail.com',
-    python_requires='>=3.6',
+    author_email="jimmiegoode@gmail.com",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Lots of tabular models running in Metaflow",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
-    keywords='metaflow_tabular',
-    name='metaflow_tabular',
-    packages=find_packages(include=['metaflow_tabular', 'metaflow_tabular.*']),
-    test_suite='tests',
+    keywords="metaflow_tabular",
+    name="metaflow_tabular",
+    packages=find_packages(include=["metaflow_tabular", "metaflow_tabular.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/jimgoo/metaflow-tabular',
-    version='0.1.0',
+    url="https://github.com/jimgoo/metaflow-tabular",
+    version="0.1.0",
     zip_safe=False,
 )
