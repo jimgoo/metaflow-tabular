2314c5cf044142004c9e97cc3693abf1178e9282
jimgoo <jimmiegoode@gmail.com>
2021-12-02 21:51:12 -0600

    Add simpler version

--- metaflow_tabular/forecasting_flow.py
+++ metaflow_tabular/forecasting_flow.py
@@ -25,35 +25,8 @@ PANDAS_VERSION = "1.3.3"
 # not required but helpful for debugging with jupyter
 IPYKERNEL_VERSION = "6.5.0"
 
+# for when we must use pip instead of conda
 PIP_VERSION = "21.3.1"
-
-
-def prediction_iterator(n_train, n_test, forecast_steps):
-    """
-    Generate a sequence of intervals for making forecasts.
-
-    `pred_interval` is a tuple of the form (start, end),
-    where start and end are integers that specify the range of
-    past time-series values to use for making a forecast.
-
-    `true_interval` is a tuple of the form (start, end),
-    where start and end are integers that specify the range of
-    true future time-series values to use for scoring.
-
-    :param int n_train: number of training samples
-    :param int n_test: number of test samples
-    :param int forecast_steps: number of steps ahead to forecast
-    :return: iterator of tuples of the form (pred_interval, true_interval)
-    """
-    i = 0
-    start, end = 0, n_train
-
-    while end < n_train + n_test - forecast_steps:
-        end = n_train + i * forecast_steps
-        pred_interval = (start, end)
-        true_interval = (end, end + forecast_steps)
-        i += 1
-        yield pred_interval, true_interval
 
 
 @conda_base(python="3.8.12")
@@ -62,10 +35,16 @@ class ForecastingFlow(FlowSpec):
     A flow for benchmarking forecasting libraries.
     """
 
-    df_path = Parameter(
-        "df_path",
-        help="The path to a DataFrame CSV file",
+    train_path = Parameter(
+        "train_path",
+        help="The path to a DataFrame file for training",
         default="https://jgoode.s3.amazonaws.com/ts-datasets/seattle-trail.csv",
+    )
+
+    test_path = Parameter(
+        "test_path",
+        help="The path to a DataFrame file for testing",
+        default=None,
     )
 
     date_col = Parameter(
@@ -84,6 +63,12 @@ class ForecastingFlow(FlowSpec):
         "model_config_path",
         help="The path to a model config file",
         default="../configs/forecasting/models/default.yaml",
+    )
+
+    forecast_steps = Parameter(
+        "forecast_steps",
+        help="The number of steps ahead to forecast",
+        default=10,
     )
 
     @conda(
@@ -107,49 +92,50 @@ class ForecastingFlow(FlowSpec):
         # Print the Metaflow metadata provider
         print(f"Using metadata provider: {get_metadata()}")
 
-        df = pd.read_csv(self.df_path)
+        def load_df(path):
+            df = pd.read_csv(path)
 
-        assert self.date_col in df.columns, '"%s" not in columns' % self.date_col
-        assert self.target_col in df.columns, '"%s" not in columns' % self.target_col
+            assert self.date_col in df.columns, '"%s" not in columns' % self.date_col
+            assert self.target_col in df.columns, (
+                '"%s" not in columns' % self.target_col
+            )
 
-        # parse date column and set it as the index
-        df[self.date_col] = pd.to_datetime(df[self.date_col])
-        df.set_index(self.date_col, inplace=True)
+            # parse date column and set it as the index
+            df[self.date_col] = pd.to_datetime(df[self.date_col])
+            df.set_index(self.date_col, inplace=True)
+
+            return df
+
+        self.train_df = load_df(self.train_path)
+        if self.test_path is not None:
+            self.test_df = load_df(self.test_path)
+            assert (
+                self.train_df.columns == self.test_df.columns
+            ).all(), "Columns do not match"
+        else:
+            self.test_df = None
+
+        if self.test_df is None:
+            n_train = 500
+            self.test_df = self.train_df.iloc[n_train : n_train + self.forecast_steps]
+            self.train_df = self.train_df.iloc[:n_train]
 
         # get index of the target column
-        self.target_index = df.columns.tolist().index(self.target_col)
+        self.target_index = self.train_df.columns.tolist().index(self.target_col)
 
         # get the frequency of the data
-        self.freq = pd.infer_freq(df.index)
-
-        # this will pickle the DataFrame for other steps to use
-        self.df = df
+        self.freq = pd.infer_freq(self.train_df.index)
 
         # load the model config file
         with open(self.model_config_path, "r") as f:
             self.model_config = yaml.safe_load(f)
 
-        # setup the backtesting windows
-        self.n_train = 500
-        self.n_test = 100
-        self.forecast_steps = 10
-
-        self.backtest_windows = [
-            dict(
-                train_start=0,
-                train_end=self.n_train,
-                pred_intervals=list(
-                    prediction_iterator(self.n_train, self.n_test, self.forecast_steps)
-                ),
-            )
-        ]
-
-        print("DataFrame")
-        print(self.df)
+        print("train df")
+        print(self.train_df)
+        print("test df")
+        print(self.test_df)
         print("model_config")
         pprint(self.model_config)
-        print("backtest_windows")
-        print(self.backtest_windows)
 
         # branches will run in parallel
         self.next(
@@ -188,38 +174,12 @@ class ForecastingFlow(FlowSpec):
                 **model_kwargs,
             )
 
-            forecasts = []
-            for iw, window in enumerate(self.backtest_windows):
-                train_ts = TimeSeries.from_pd(
-                    self.df.iloc[window["train_start"] : window["train_end"]]
-                )
+            train_ts = TimeSeries.from_pd(self.train_df)
+            model.train(train_ts)
 
-                model.reset()
-                model.train(train_ts)
-
-                for ip, (pred_interval, true_inverval) in enumerate(
-                    window["pred_intervals"]
-                ):
-                    prev = self.df.iloc[pred_interval[0] : pred_interval[1]]
-                    true = self.df.iloc[true_inverval[0] : true_inverval[1]]
-
-                    forecast, err = model.forecast(
-                        true.index, time_series_prev=TimeSeries.from_pd(prev)
-                    )
-                    forecast = forecast.to_pd().values
-                    # err = err.to_pd() if err is not None else None
-
-                    forecasts.append(
-                        dict(
-                            id=config["id"],
-                            iw=iw,
-                            ip=ip,
-                            true=true.values,
-                            pred=forecast,
-                        )
-                    )
-
-            return forecasts
+            forecast, err = model.forecast(self.forecast_steps)
+            forecast = forecast.to_pd().values
+            return dict(id=config["id"], forecast=forecast)
 
         self.forecasts = parallel_map(
             run_model, self.model_config["libs"].get("merlion", [])
@@ -243,19 +203,19 @@ class ForecastingFlow(FlowSpec):
         from gluonts.model.forecast import SampleForecast
         from gluonts.mx.trainer import Trainer
 
+        def df_to_dset(df):
+            return ListDataset(
+                [
+                    {
+                        "start": df.index[0],
+                        "target": df.values[:, self.target_index],
+                    },
+                ],
+                freq=self.freq,
+            )
+
         def run_model(config):
             print(f"Running: {config}")
-
-            def df_to_dset(df):
-                return ListDataset(
-                    [
-                        {
-                            "start": df.index[0],
-                            "target": df.values[:, self.target_index],
-                        },
-                    ],
-                    freq=self.freq,
-                )
 
             EstimatorClass = locate(config["estimator_class"])
 
@@ -280,46 +240,23 @@ class ForecastingFlow(FlowSpec):
                     **config.get("estimator_kwargs", {}),
                 )
 
-            forecasts = []
-            for iw, window in enumerate(self.backtest_windows):
-                train_dset = df_to_dset(
-                    self.df.iloc[window["train_start"] : window["train_end"]]
-                )
+            train_dset = df_to_dset(self.train_df)
+            predictor = estimator.train(training_data=train_dset)
 
-                predictor = estimator.train(training_data=train_dset)
+            # An iterator over the number of targets (only one for univariate)
+            forecast = predictor.predict(train_dset, num_samples=100)
 
-                for ip, (pred_interval, true_inverval) in enumerate(
-                    window["pred_intervals"]
-                ):
-                    prev = self.df.iloc[pred_interval[0] : pred_interval[1]]
-                    true = self.df.iloc[true_inverval[0] : true_inverval[1]]
+            # For models like DeepAR, will be (num_samples x self.forecast_steps),
+            # for models without sampling, will be (1 x self.forecast_steps).
+            forecast = next(forecast)
 
-                    prev_dset = df_to_dset(prev)
+            # some models return QuantileForecast instead
+            assert isinstance(forecast, SampleForecast)
 
-                    # An iterator over the number of targets (only one for univariate)
-                    forecast = predictor.predict(prev_dset, num_samples=100)
+            # take mean over samples
+            forecast = np.mean(forecast.samples, 0)
 
-                    # For models like DeepAR, will be (num_samples x self.forecast_steps),
-                    # for models without sampling, will be (1 x self.forecast_steps).
-                    forecast = next(forecast)
-
-                    # some models return QuantileForecast instead
-                    assert isinstance(forecast, SampleForecast)
-
-                    # take mean over samples
-                    forecast = np.mean(forecast.samples, 0)
-
-                    forecasts.append(
-                        dict(
-                            id=config["id"],
-                            iw=iw,
-                            ip=ip,
-                            true=true.values,
-                            pred=forecast,
-                        )
-                    )
-
-            return forecasts
+            return dict(id=config["id"], forecast=forecast)
 
         self.forecasts = parallel_map(
             run_model, self.model_config["libs"].get("gluonts", [])
@@ -338,50 +275,27 @@ class ForecastingFlow(FlowSpec):
         from pydoc import locate
         from kats.consts import TimeSeriesData
 
+        def df_to_data(df):
+            df2 = df.iloc[:, [self.target_index]].reset_index()
+            df2.columns = ["time", "value"]
+            return TimeSeriesData(df2)
+
         def run_model(config):
             print(f"Running: {config}")
-
-            def df_to_data(df):
-                df2 = df.iloc[:, [self.target_index]].reset_index()
-                df2.columns = ["time", "value"]
-                return TimeSeriesData(df2)
 
             ModelClass = locate(config["model_class"])
             ParamsClass = locate(config["params_class"])
 
             params = ParamsClass(**config.get("params_kwargs", {}))
+            train_data = df_to_data(self.train_df)
+            model = ModelClass(train_data, params)
+            model.fit()
 
-            forecasts = []
-            for iw, window in enumerate(self.backtest_windows):
-                train_data = df_to_data(
-                    self.df.iloc[window["train_start"] : window["train_end"]]
-                )
+            # some models have columns "fcst_lower" and "fcst_upper" for upper and lower bounds
+            forecast = model.predict(steps=self.forecast_steps, freq=self.freq)
+            forecast = forecast["fcst"].values
 
-                model = ModelClass(train_data, params)
-                model.fit()
-
-                for ip, (pred_interval, true_inverval) in enumerate(
-                    window["pred_intervals"]
-                ):
-                    # prev = self.df.iloc[pred_interval[0] : pred_interval[1]]
-                    true = self.df.iloc[true_inverval[0] : true_inverval[1]]
-
-                    # TODO: allow for using prev data
-                    forecast = model.predict(steps=self.forecast_steps, freq=self.freq)
-                    # some models have columns "fcst_lower" and "fcst_upper" for upper and lower bounds
-                    forecast = forecast["fcst"].values
-
-                    forecasts.append(
-                        dict(
-                            id=config["id"],
-                            iw=iw,
-                            ip=ip,
-                            true=true.values,
-                            pred=forecast,
-                        )
-                    )
-
-            return forecasts
+            return dict(id=config["id"], forecast=forecast)
 
         self.forecasts = parallel_map(
             run_model, self.model_config["libs"].get("kats", [])
@@ -401,50 +315,25 @@ class ForecastingFlow(FlowSpec):
         from pydoc import locate
         from neuralprophet import NeuralProphet
 
+        def convert_df(df):
+            # requires a column named "ds" for dates and "y" for target
+            df2 = df.iloc[:, [self.target_index]].reset_index()
+            df2.columns = ["ds", "y"]
+            return df2
+
         def run_model(config):
             print(f"Running: {config}")
 
-            def convert_df(df):
-                # requires a column named "ds" for dates and "y" for target
-                df2 = df.iloc[:, [self.target_index]].reset_index()
-                df2.columns = ["ds", "y"]
-                return df2
+            train_df = convert_df(self.train_df)
 
-            forecasts = []
-            for iw, window in enumerate(self.backtest_windows):
-                train_df = self.df.iloc[window["train_start"] : window["train_end"]]
-                train_df = convert_df(train_df)
+            model = NeuralProphet(**config.get("model_kwargs", {}))
+            model.fit(train_df, freq=self.freq)
 
-                model = NeuralProphet(**config.get("model_kwargs", {}))
-                model.fit(train_df, freq=self.freq)
+            future = model.make_future_dataframe(train_df, periods=self.forecast_steps)
+            forecast = model.predict(future)
+            forecast = forecast["yhat1"].values
 
-                for ip, (pred_interval, true_inverval) in enumerate(
-                    window["pred_intervals"]
-                ):
-                    prev = self.df.iloc[pred_interval[0] : pred_interval[1]]
-                    true = self.df.iloc[true_inverval[0] : true_inverval[1]]
-
-                    prev = convert_df(prev)
-
-                    future = model.make_future_dataframe(
-                        prev, periods=self.forecast_steps
-                    )
-                    print("---")
-                    print(future)
-                    forecast = model.predict(future)
-                    forecast = forecast["yhat1"].values
-
-                    forecasts.append(
-                        dict(
-                            id=config["id"],
-                            iw=iw,
-                            ip=ip,
-                            true=true.values,
-                            pred=forecast,
-                        )
-                    )
-
-            return forecasts
+            return dict(id=config["id"], forecast=forecast)
 
         self.forecasts = parallel_map(
             run_model, self.model_config["libs"].get("neuralprophet", [])
@@ -461,34 +350,58 @@ class ForecastingFlow(FlowSpec):
         import numpy as np
         import pandas as pd
 
-        errors = OrderedDict()
-        rmses = OrderedDict()
-        for inp in inputs:
-            for model_windows in inp.forecasts:
-                error = np.zeros((inp.forecast_steps, 1))
-                for window in model_windows:
-                    # univariate scoring
-                    true = window["true"][:, inp.target_index].reshape(-1, 1)
-                    pred = window["pred"].reshape(-1, 1)
-                    error += (pred - true) ** 2
-                    model_id = window["id"]  # all have same id
+        forecasts = OrderedDict()
+        for lib in inputs:
+            self.test_df = lib.test_df
+            self.target_index = lib.target_index
+            for forecast in lib.forecasts:
+                assert (
+                    forecast["id"] not in forecasts
+                ), f"Duplicate forecast id: {forecast['id']}"
+                forecasts[forecast["id"]] = forecast["forecast"].reshape(-1)
 
-                rmse = np.sqrt(np.mean(error))
-                errors[model_id] = error
-                rmses[model_id] = rmse
-                # TODO: handle multiple retrainings
+        self.forecasts = pd.DataFrame(forecasts)
+        print(self.forecasts)
 
-        print(pd.Series(rmses, name="RMSE").sort_values())
+        if self.test_df is not None:
+            # duplicate univariate target across columns for each model
+            true = self.test_df.iloc[
+                : self.forecast_steps, [self.target_index] * self.forecasts.shape[1]
+            ]
+            pred = self.forecasts
+            print(true)
+            print("--")
+            print(pred)
+            rmse = np.sqrt(np.mean((pred.values - true.values) ** 2, 1))
+            step_rmse = pd.DataFrame(np.sqrt((pred.values - true.values) ** 2))
+            print(f"RMSE: {rmse}")
+            print(f"Step RMSE:")
+            print(step_rmse)
 
-        self.errors = errors
-        self.rmses = rmses
+        #         error = np.zeros((inp.forecast_steps, 1))
+        #         for window in model_windows:
+        #             # univariate scoring
+        #             true = window["true"][:, inp.target_index].reshape(-1, 1)
+        #             pred = window["pred"].reshape(-1, 1)
+        #             error += (pred - true) ** 2
+        #             model_id = window["id"]  # all have same id
+
+        #         rmse = np.sqrt(np.mean(error))
+        #         errors[model_id] = error
+        #         rmses[model_id] = rmse
+        #         # TODO: handle multiple retrainings
+
+        # print(pd.Series(rmses, name="RMSE").sort_values())
+
+        # self.errors = errors
+        # self.rmses = rmses
 
         self.next(self.end)
 
     @step
     def end(self):
         """
-        Cleanup and exit.
+        End of the flow
         """
         pass
 
